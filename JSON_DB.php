1fa6ce8740b201e3cdc3c4d81d6c17dbe9b136d6
Jhordy Abonia <jeigl7@gmail.com>
2019-05-24 15:03:12 -0500

    ready the function test and function rows

--- JSON_DB.php
+++ JSON_DB.php
@@ -120,6 +120,9 @@ class JSON_DB{
         }
         return $out;
     }
+    public function rows(){
+        return $this->database->rows;
+    }
     public function commit(){
         $fh = fopen($this->fileName, 'w');
         $data = json_encode($this->database,JSON_PRETTY_PRINT);
@@ -127,24 +130,27 @@ class JSON_DB{
         fclose($fh);
     }
 }
-$db = new JSON_DB("file","id,name,opacity","id");
-/*$db->insert(['id'=>1,'name'=>'none0']);
-$db->insert(['id'=>2,'name'=>'none1']);
-$db->insert(['id'=>3,'name'=>'none2']);
-$db->insert(['id'=>4,'name'=>'test0']);
-$db->insert(['id'=>5,'name'=>'test1']);
-$db->insert(['id'=>6,'name'=>'test2']);*/
-$db->insert(['id'=>7,'color'=>'#FF0000','name'=>'red']);
-$db->update(['id'=>7],['color'=>'#0000FF','name'=>'blue']);
-$db->updateLine(6,['color'=>'#00FF00','name'=>'green','opacity'=>0.1]);
+function test(){
+    $db = new JSON_DB("file","id,name,opacity","id");
+    $db->insert(['id'=>1,'name'=>'none0']);
+    $db->insert(['id'=>2,'name'=>'none1']);
+    $db->insert(['id'=>3,'name'=>'none2']);
+    $db->insert(['id'=>4,'name'=>'test0']);
+    $db->insert(['id'=>5,'name'=>'test1']);
+    $db->insert(['id'=>6,'name'=>'test2']);
+    $db->insert(['id'=>7,'color'=>'#FF0000','name'=>'red']);
+    $db->update(['id'=>7],['color'=>'#0000FF','name'=>'blue']);
+    $db->updateLine(6,['color'=>'#00FF00','name'=>'green','opacity'=>0.1]);
 
-/*$db->remove(['id'=>'2'])."\n";
-$db->remove(['name'=>'none0'])."\n";
-$db->remove(['name'=>'none'],true)."\n";
-$db->remove(['name'=>'test'],true)."\n";*/
+    /*$db->remove(['id'=>'2'])."\n";
+    $db->remove(['name'=>'none0'])."\n";
+    $db->remove(['name'=>'none'],true)."\n";
+    $db->remove(['name'=>'test'],true)."\n";*/
 
-$db->commit();
+    $db->commit();
 
-//print_r($db->getAll());
-print_r($db->get(['id'=>7]));
+    //print_r($db->getAll());
+    print_r($db->get(['id'=>7]));
+}
+//test();
 ?>
